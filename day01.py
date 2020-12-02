45ab62962c6e353050e2aa9a0eb06ceccf730a9b
Jorge Aranda <jorge.aranda@cuevano.ca>
2020-12-02 10:24:36 -0800

    Refactor day 1

--- day01.py
+++ day01.py
@@ -11,29 +11,23 @@ def load_input(filename):
         return [int(line) for line in f.readlines()]
 
 
-def find_two_entry_product(report):
-    for pair in combinations(report, 2):
-        if sum(pair) == SUM_TO:
-            return prod(pair)
-
-
-def find_three_entry_product(report):
-    for triple in combinations (report, 3):
-        if sum(triple) == SUM_TO:
-            return prod(triple)
+def find_match_product(report, items):
+    for combination in combinations(report, items):
+        if sum(combination) == SUM_TO:
+            return prod(combination)
 
 
 if __name__ == "__main__":
     report = load_input("day01_input.txt")
-    print(f"Part 1: {find_two_entry_product(report)}")
-    print(f"Part 2: {find_three_entry_product(report)}")
+    print(f"Part 1: {find_match_product(report, 2)}")
+    print(f"Part 2: {find_match_product(report, 3)}")
 
 
 def test_sample_part_1():
     report = [1721, 979, 366, 299, 675, 1456]
-    assert find_two_entry_product(report) == 514579
+    assert find_match_product(report, 2) == 514579
 
 
 def test_sample_part_2():
     report = [1721, 979, 366, 299, 675, 1456]
-    assert find_three_entry_product(report) == 241861950
+    assert find_match_product(report, 3) == 241861950
